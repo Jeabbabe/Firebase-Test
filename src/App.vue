f933a86b76469e04ed8e031fb1f5820ae55ad0ee
Your Name <you@example.com>
2023-03-03 22:56:18 +0700

    renovated

--- src/App.vue
+++ src/App.vue
@@ -3,63 +3,41 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink to="/"><v-btn>Home</v-btn></RouterLink>
-        <RouterLink to="/about"><v-btn>About</v-btn></RouterLink>
-        <RouterLink to="/login"><v-btn>Log In</v-btn></RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-layout>
+    <v-system-bar>
+      <v-icon icon="mdi-wifi-strength-4"></v-icon>
+      <v-icon icon="mdi-signal" class="ms-2"></v-icon>
+      <v-icon icon="mdi-battery" class="ms-2"></v-icon>
 
-  <RouterView />
+      <span class="ms-2">3:13PM</span>
+    </v-system-bar>
+
+    <v-app-bar color="brown" image="../src/components/images/thai-food10.jpg">
+      <template v-slot:image>
+        <v-img gradient="to top right, rgba(128,64,0,.8), rgba(92,56,0,.8)"></v-img>
+      </template>
+      <v-app-bar-title>หน้าเว็บสั่งเมนู</v-app-bar-title>
+      <v-spacer />
+      <div class="container">
+        <RouterLink to="/">
+          <v-btn color="orange" size="large" variant="elevated">Home</v-btn>
+        </RouterLink>
+        <RouterLink to="/order">
+          <v-btn color="orange" size="large" variant="elevated">Order</v-btn>
+        </RouterLink>
+        <RouterLink to="/login">
+          <v-btn color="orange" size="large" variant="elevated">Log In</v-btn>
+        </RouterLink>
+      </div>
+    </v-app-bar>
+    <v-main style="margin-block: 2rem"><RouterView /></v-main>
+  </v-layout>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  nav {
-    text-align: center;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.container {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  gap: 10px;
 }
 </style>
